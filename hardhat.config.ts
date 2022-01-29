15f13637feca8df9797ee9ae711666ab84f354d3
Shahan Khatchadourian <shahan.k.code@gmail.com>
2022-01-29 08:32:27 -0500

    [feat] add hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,10 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+    paths: {
+        sources: "./contracts",
+        artifacts: "./artifacts/contracts",
+    },
+    solidity: "0.6.12",
+};
